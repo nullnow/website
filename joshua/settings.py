79979455fcae9164900d00bb3b193adcc719569e
Usheninte Joshua Dangana <usheninte234@gmail.com>
2026-03-25 17:56:30 +0100

    updated allowed hosts configuration

--- joshua/settings.py
+++ joshua/settings.py
@@ -36,7 +36,7 @@ DEBUG = bool(env("DEBUG"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
