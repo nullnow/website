873c2e16d027212a1c3cfa176448f433056c651c
Usheninte Joshua Dangana <usheninte234@gmail.com>
2026-03-25 17:55:35 +0100

    Added settings for static files

--- joshua/settings.py
+++ joshua/settings.py
@@ -143,6 +143,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 # Compressed, but not cached.
 # This is preferred for ease of usage in simple deployments.
 
